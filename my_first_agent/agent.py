50543656f93d434dd70c3774399c28e79c02e08c
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2026-02-04 01:29:41 +0100

    import module

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -1,4 +1,8 @@
 from google.adk.agents.llm_agent import Agent
+from google.adk.tools import google_search
+from wsgiref import types
+from pydantic import BaseModel, Field
+
 
 def greeting_tool() -> str:
     """Returns a warm friendly greeting."""
@@ -9,5 +13,15 @@ root_agent = Agent(
     name='root_agent',
     description='A friendly agent that provides a special greeting.',
     instruction='You are a friendly agent. When the user greets you, you must use the greeting_tool to respond.',
-    tools=[greeting_tool],
+    tools=[google_search],
+    generate_content_config=types.GenerateContentConfig(
+        temperature=0.2,
+        max_output_tokens=250,
+        safety_settings=[
+            types.SafetySetting(
+                category=types.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+                threshold=types.HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+            )
+        ]
+    )
 )
